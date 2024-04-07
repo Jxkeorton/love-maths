20933f218a2c727733f6dd87c98c124868f1bfe2
Jxkeorton <jakeorton99@gmail.com>
2024-04-07 10:19:23 +0100

    functions added to js

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+#
+function incrementScore(){
+
+}
+
+function incrementWromngAnswer(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+
+}
+
+function displayMultiplyQuestion(){
+
+}
+
+function displayDivideQuestion(){
+    
+}
